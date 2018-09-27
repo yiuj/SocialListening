700bec6eb44d848c65b62233fdfb1d3893a77360
Will Nee <Will@Wills-MacBook-Pro-3.local>
2018-09-27 14:37:42 -0700

    adding twitter projects

---
+++ code3 copy/hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "vFutoJoPIDaF8O3U3ifJUnrFw",
+            "consumer_secret": "Gfy8ykTjnHf7gxj0kkakRWqY9gWhDEd69aHDWewAWP2hsxCv0z",
+            "token_key": "855588527039422465-BirmEI78Vvugvm44y5cJzNoMjPiF8B8",
+            "token_secret": "RjZhI9lCoptKipRcKnPV6Uu4mIa4L0CGk9FoBXJ7GJNnB"}
